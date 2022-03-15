f1e8901b4c2172b7f251ec6bd08f9a97fa644009
Naoto-110616 <siodeitadaku@gmail.com>
2022-03-15 22:17:39 +0900

    #1 Managing and Naming Routes

--- practice/laravel/routes/web.php
+++ practice/laravel/routes/web.php
@@ -15,4 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return "Home Page";
-});
+})->name("home.index");
+Route::get("/contact", function () {
+    return "Contact";
+})->name("home.contact");
